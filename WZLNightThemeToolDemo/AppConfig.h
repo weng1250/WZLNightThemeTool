e52e804a05b9d246e75075096accb7a8866693ff
zilin.wzl <zilin.wzl@alibaba-inc.com>
2016-04-01 14:43:16 +0800

    add appConfig into demo project

---
+++ WZLNightThemeToolDemo/AppConfig.h
@@ -0,0 +1,13 @@
+//
+//  AppConfig.h
+//  WZLNightThemeToolDemo
+//
+//  Created by wengzilin on 16/4/1.
+//  Copyright © 2016年 Weng-Zilin(http://www.cnblogs.com/wengzilin/). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppConfig : NSObject
+
+@end
---
+++ WZLNightThemeToolDemo/AppConfig.m
@@ -0,0 +1,13 @@
+//
+//  AppConfig.m
+//  WZLNightThemeToolDemo
+//
+//  Created by wengzilin on 16/4/1.
+//  Copyright © 2016年 Weng-Zilin(http://www.cnblogs.com/wengzilin/). All rights reserved.
+//
+
+#import "AppConfig.h"
+
+@implementation AppConfig
+
+@end
